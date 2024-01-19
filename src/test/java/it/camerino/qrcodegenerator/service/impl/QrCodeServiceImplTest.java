9f234c61234301640fa02e30dcfaa6055640cbdf
Nurdin Abdrasulov <01mail.kg@gmail.com>
2024-01-19 20:01:08 +0100

    added a few extra unit tests

--- src/test/java/it/camerino/qrcodegenerator/service/impl/QrCodeServiceImplTest.java
+++ src/test/java/it/camerino/qrcodegenerator/service/impl/QrCodeServiceImplTest.java
@@ -1,30 +1,67 @@
 package it.camerino.qrcodegenerator.service.impl;
 
 import it.camerino.qrcodegenerator.dto.QrCodeDto;
-import org.mockito.internal.matchers.Equals;
+import it.camerino.qrcodegenerator.entity.QrCode;
+import it.camerino.qrcodegenerator.entity.User;
+import it.camerino.qrcodegenerator.repository.QrCodeRepo;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import org.junit.jupiter.api.condition.EnabledOnOs;
-
-import static org.junit.jupiter.api.condition.OS.LINUX;
-import static org.junit.jupiter.api.condition.OS.MAC;
-import static org.junit.jupiter.api.condition.OS.WINDOWS;
-
+@ExtendWith(MockitoExtension.class)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class QrCodeServiceImplTest {
+
+
+    @Mock
+    QrCodeRepo repo;
+
+    @InjectMocks
+    QrCodeServiceImpl service;
+
     @Test
-    public void CreateShouldNotReturnZero()
-    {
+    public void CreateShouldNotReturnZero() {
         QrCodeDto dto = new QrCodeDto();
 
         assertNotNull(dto);
+    }
+
+    @Test
+    public void shouldThorw() {
+        //given
+        final long qrId = 111L;
+        final String expectedExceptionMessage = "Method is not implemented";
+
+        //when
+        //then
+        UnsupportedOperationException unsupportedOperationException = assertThrows(UnsupportedOperationException.class, () -> service.delete(qrId));
+        assertEquals(expectedExceptionMessage, unsupportedOperationException.getMessage());
+    }
+
+
+    @Test
+    public void shouldCallSaveMethodOneTime() {
+        //given
+        final QrCodeDto dto = QrCodeDto.builder()
+                .color("blue")
+                .link("https://www.google.com/")
+                .build();
+
+        final User currentUser = new User();
+
+        //when
+        service.create(dto, currentUser);
+
+        //then
+        verify(repo, times(1)).save(any());
     }
 }
